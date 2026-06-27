6d0ea2b04bb44e7d2f4ddc6e8297b7d8e984ddfd
Alessandro Donnini <alaribus@gmail.com>
2026-06-27 11:53:52 +0200

    Aggiunte descrizioni campioni

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const campioni = [
 nome:"Kargan",
 classe:"Barbaro",
 immagine:"images/kargan.jpg",
+
+flavour:"Capotribù del Lupo Bianco, guida il suo clan con forza e onore.",
+fight:"Travolge il nemico in prima linea, assorbe danni e domina il corpo a corpo."
+
 facilita:5,
 danni:5,
 difesa:4,
@@ -16,6 +20,10 @@ supporto:1
 nome:"Vivienne Fox",
 classe:"Ladro",
 immagine:"images/vivienne.jpg",
+
+flavour:"Ladra elfica dalle misteriose lame psioniche, elegante e letale.",
+fight:"Colpisce i bersagli isolati con rapidità, sfruttando mobilità e precisione."
+
 facilita:3,
 danni:4,
 difesa:3,
@@ -28,6 +36,10 @@ supporto:1
 nome:"Dantu Lighierù",
 classe:"Bardo",
 immagine:"images/dantu.jpg",
+
+flavour:"Spadaccino e artista, trasforma ogni duello in uno spettacolo.",
+fight:"Alterna magia e scherma, adattandosi a ogni situazione senza perdere ritmo."
+
 facilita:3,
 danni:3,
 difesa:3,
@@ -40,6 +52,10 @@ supporto:3
 nome:"Kaede Akatsuki, la Rinnegata",
 classe:"Monaco",
 immagine:"images/kaede.jpg",
+
+flavour:"Shinobi rinnegata che ha trasformato la disciplina in un'arma.",
+fight:"Controlla il campo con velocità, colpi mirati e continue riposizioni."
+
 facilita:3,
 danni:4,
 difesa:4,
@@ -52,6 +68,10 @@ supporto:2
 nome:"Sir Rhogar",
 classe:"Mago",
 immagine:"images/rhogar.jpg",
+
+flavour:"Aristocratico maestro della lama, unisce magia e scherma in perfetta armonia.",
+fight:"Alterna incantesimi e fendenti con estrema eleganza e precisione."
+
 facilita:2,
 danni:5,
 difesa:3,
@@ -64,6 +84,10 @@ supporto:2
 nome:"Rask 'Red Dust'",
 classe:"Artefice",
 immagine:"images/rask.jpg",
+
+flavour:"Geniale inventore coboldo, sempre pronto a stupire con le sue creazioni.",
+fight:"Combatte dalla distanza con cannoni e marchingegni, controllando il campo."
+
 facilita:2,
 danni:4,
 difesa:3,
@@ -76,6 +100,10 @@ supporto:4
 nome:"Daphne, Strega Cremisi",
 classe:"Stregone",
 immagine:"images/daphne.jpg",
+
+flavour:"Strega cremisi legata al potere dei draghi e delle fiamme.",
+fight:"Infligge enormi danni magici e punisce i nemici con potenti esplosioni."
+
 facilita:3,
 danni:5,
 difesa:2,
@@ -88,6 +116,10 @@ supporto:2
 nome:"Melkor",
 classe:"Chierico",
 immagine:"images/melkor.jpg",
+
+flavour:"Gigante goliath consacrato alla guerra e alla vittoria.",
+fight:"Sostiene il gruppo con la fede, resiste agli assalti e schiaccia i nemici in prima linea."
+
 facilita:4,
 danni:4,
 difesa:5,
@@ -100,6 +132,10 @@ supporto:3
 nome:"Kael Mord, Redivivo",
 classe:"Warlock",
 immagine:"images/kael.jpg",
+
+flavour:"Un redivivo che ha stretto un oscuro patto per sfuggire alla morte.",
+fight:"Logora lentamente gli avversari con magie oscure e un controllo costante."
+
 facilita:2,
 danni:4,
 difesa:2,
@@ -112,6 +148,10 @@ supporto:3
 nome:"Pin, lo Stelo",
 classe:"Ranger",
 immagine:"images/pin.jpg",
+
+flavour:"Piccolo esploratore dal grande coraggio, inseparabile dalle sue bestie.",
+fight:"Colpisce da lontano coordinandosi con la sua bestia per mettere in difficoltà il nemico."
+
 facilita:4,
 danni:4,
 difesa:3,
@@ -124,6 +164,10 @@ supporto:3
 nome:"Toren, Primo Albero",
 classe:"Druido",
 immagine:"images/toren.jpg",
+
+flavour:"Guardiano firbolg della natura, saldo come una quercia secolare.",
+fight:"Si trasforma per dominare la mischia e protegge gli alleati con la forza della natura."
+
 facilita:2,
 danni:4,
 difesa:4,
@@ -136,6 +180,10 @@ supporto:5
 nome:"Harlan, il Mastino",
 classe:"Guerriero",
 immagine:"images/harlan.jpg",
+
+flavour:"Veterano instancabile, celebre per la sua freddezza sul campo di battaglia.",
+fight:"Controlla il ritmo dello scontro con tecniche marziali e manovre tattiche."
+
 facilita:3,
 danni:3,
 difesa:4,
@@ -148,6 +196,12 @@ supporto:2
 nome:"Thalia",
 classe:"Paladino",
 immagine:"images/thalia.jpg",
+
+
+flavour:"Campionessa aasimar della Gloria, simbolo di coraggio e speranza.",
+fight:"Difende i compagni e guida l'assalto con resistenza e potenza divina."
+
+
 facilita:4,
 danni:4,
 difesa:4,
